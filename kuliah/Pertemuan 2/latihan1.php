d999406fdb9b1568d231ecff69927decb2eef74d
awithliee <alijr282006@gmail.com>
2025-02-17 17:35:56 +0700

    first commit

---
+++ kuliah/Pertemuan 2/latihan1.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Coba pehape</title>
+</head>
+<body>
+    <h1><?php echo "Hello World!"; ?></h1>
+    <h1>Nama saya : <?php echo "M Ali Fauzi JR!"; ?></h1>
+    <h1>Alamat : <?php echo "Di mana aja"; ?></h1>
+</body>
+</html>
+
+<!--echo di gunakan untuk mencetak nilai-->
+<!--jika tanpa "" jadi bilangan -->
---
+++ kuliah/Pertemuan 2/latihan2.php
@@ -0,0 +1,21 @@
+<?php
+ $nama ="Ali fauzi";
+$email = "alifauzi@gmail.com";
+$jurusan = "TIF";
+$status = "MAHASISWA";
+
+echo "<h1>$nama</h1>";
+echo "<p>$email</p>";
+
+echo "Halo nama saya $nama, Saya adalah $status, dari $jurusan UNPAS angkatan 24.";
+
+?>
+
+<!-----tidak perlu doctype html----->
+<!-----Karena sudah di buat oleh browser----->
+<!-----Jika ingin pake css harus buat html dulu----->
+<!-----cara mengambil data dengan mudah menggunakan sebuah variabel----->
+<!----membuat variabel di php dengan "$"---------->
+<!----perhatikan pengetikan huruf nya-->
+<!---tidak boleh ada spasi dalam penulisan dalam variabel--->
+<!--  -->
