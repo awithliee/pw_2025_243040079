b41bb131b594e14b2b35c2490d8810ddce8f5826
awithliee <alijr282006@gmail.com>
2025-02-25 08:27:01 +0700

    commit tugas ke 2

--- tugas/tugas2/2c.php
+++ tugas/tugas2/2c.php
@@ -1,1 +1,39 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <title>Tugas 1c</title>
+    <style>
+        table {
+            border-collapse: collapse;
+            margin: 20px;
+        }
+        td {
+            border: 2px solid;
+            border-color: black;
+            background-color: lightblue;
+            padding: 10px;
+            text-align: center;
+            width: 30px;
+            height: 30px;
+        }
+    </style>
+</head>
+<body>
 
+
+<table>
+    <?php
+    $jumlahBaris = 10; // Jumlah baris
+    for ($i = 1; $i <= $jumlahBaris; $i++) {
+        echo "<tr>";
+        for ($j = 1; $j <= $jumlahBaris - $i + 1; $j++) {
+            echo "<td>" . $j . "</td>";
+        }
+        echo "</tr>";
+    }
+    ?>
+</table>
+
+</body>
+</html>
