577e8dc0e2124be69810d9488de5714992aa2d0a
awithliee <alijr282006@gmail.com>
2025-04-01 18:01:00 +0700

    selesai mengerjakan tugas 6

--- tugas/tugas 6/GET & POST/6a.php
+++ tugas/tugas 6/GET & POST/6a.php
@@ -3,24 +3,24 @@
 
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>GET</title>
     <style>
-        table {
-            border-collapse: collapse;
-        }
-
-        td {
-            border: 2px solid;
-            border-color: black;
-            padding: 10px;
+        .box {
+            display: inline-block;
+            width: 30px;
+            height: 30px;
+            line-height: 30px;
             text-align: center;
+            border: 1px solid black;
+            font-size: 18px;
         }
 
-        tr:nth-child(odd) td {
-            background-color: lightblue;
+        .green {
+            background-color: darkgreen;
         }
 
-        tr:nth-child(even) td {
+        .white {
             background-color: white;
         }
     </style>
@@ -28,22 +28,17 @@
 
 <body>
 
-    <table>
-        <?php
-        if (isset($_GET['jumlahbaris'])) {
-            $jumlahbaris = (int)$_GET['jumlahbaris']; 
-            for ($i = $jumlahbaris; $i >= 1; $i--) {
-                echo "<tr>";
-                for ($j = 1; $j <= $i; $j++) {
-                    $color = ($i % 2 == 0) ? 'blue' : 'white';
-                    echo "<td class='$color'>$i</td>";
-                }
-                echo "</tr>";
-            }
+    <?php
+
+    $angka = isset($_GET['angka']) ? (int)$_GET['angka'] : 10;
+    for ($i = $angka; $i >= 1; $i--) {
+        for ($j = 1; $j <= $i; $j++) {
+            $color = ($i % 2 == 0) ? 'green' : 'white';
+            echo "<div class='box $color'>$i</div>";
         }
-        ?>
-    </table>
-    echo "jika ingin menampilkan baris gunakan ''?jumlahbaris=10'' di akhir url";
+        echo "<br>";
+    }
+    ?>
 
 </body>
 
--- tugas/tugas 6/GET & POST/6b.php
+++ tugas/tugas 6/GET & POST/6b.php
@@ -1,51 +1,46 @@
-<?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $angka = intval($_POST['angka']);
-} else {
-    $angka = 0;
-}
-?>
-
 <!DOCTYPE html>
 <html lang="id">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>POST</title>
     <style>
-        table {
-            border-collapse: collapse;
-        }
-        td {
-            border: 1px solid #000;
+        .box {
+            display: inline-block;
             width: 30px;
             height: 30px;
+            line-height: 30px;
             text-align: center;
+            border: 1px solid black;
+            margin: 2px;
+            font-size: 18px;
         }
-        tr:nth-child(even) td {
-            background-color: lightblue;
-            color: white;
+
+        .red {
+            background-color: tomato;
+        }
+
+        .white {
+            background-color: white;
         }
     </style>
 </head>
-<body>
-    <h2>Masukkan Angka:</h2>
-    <form method="post" action="">
-        <input type="text" name="angka" required>
-        <input type="submit" value="Tampilkan">
-    </form>
 
-    <?php if ($angka > 0): ?>
-        <h3>Hasil:</h3>
-        <table>
-            <?php for ($i = $angka; $i >= 1; $i--): ?>
-                <tr>
-                    <?php for ($j = 1; $j <= $i; $j++): ?>
-                        <td><?php echo $i; ?></td>
-                    <?php endfor; ?>
-                </tr>
-            <?php endfor; ?>
-        </table>
-    <?php endif; ?>
+<body>
+
+    <?php
+
+    $angka = isset($_GET['angka']) ? (int)$_GET['angka'] : 10;
+    for ($i = $angka; $i >= 1; $i--) {
+        for ($j = 1; $j <= $i; $j++) {
+            $color = ($i % 2 == 0) ? 'red' : 'white';
+            echo "<div class='box $color'>$i</div>";
+        }
+        echo "<br>";
+    }
+    ?>
+
 </body>
+
 </html>
